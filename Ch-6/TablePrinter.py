17a625f61ae0ca507089f4c56396ab0e83fbf066
shivamkhatter <shivamkhatter04@gmail.com>
2023-01-26 23:25:51 +0530

    hi

---
+++ Ch-6/TablePrinter.py
@@ -0,0 +1,11 @@
+tableData = [['apples','oranges','cherries','banana'],
+             ['Alice','Bob','Carol','David'],
+             ['dogs','cats','moose','goose']]
+def printTable():
+    for i in range (4):
+        for j in range(3):
+            if printTable!='':
+                print(tableData[j][i].rjust(8),end='')
+        print('\n')
+printTable()
+    
